5f072be1b1455755cedc09539d18489eb87c496e
unknown <berkeleyplatte989@gmail.com>
2019-09-11 23:05:00 -0500

    whatever

---
+++ main.js
@@ -0,0 +1,122 @@
+let n = 100;
+let ar = [
+  50,
+  49,
+  38,
+  49,
+  78,
+  36,
+  25,
+  96,
+  10,
+  67,
+  78,
+  58,
+  98,
+  8,
+  53,
+  1,
+  4,
+  7,
+  29,
+  6,
+  59,
+  93,
+  74,
+  3,
+  67,
+  47,
+  12,
+  85,
+  84,
+  40,
+  81,
+  85,
+  89,
+  70,
+  33,
+  66,
+  6,
+  9,
+  13,
+  67,
+  75,
+  42,
+  24,
+  73,
+  49,
+  28,
+  25,
+  5,
+  86,
+  53,
+  10,
+  44,
+  45,
+  35,
+  47,
+  11,
+  81,
+  10,
+  47,
+  16,
+  49,
+  79,
+  52,
+  89,
+  100,
+  36,
+  6,
+  57,
+  96,
+  18,
+  23,
+  71,
+  11,
+  99,
+  95,
+  12,
+  78,
+  19,
+  16,
+  64,
+  23,
+  77,
+  7,
+  19,
+  11,
+  5,
+  81,
+  43,
+  14,
+  27,
+  11,
+  63,
+  57,
+  62,
+  3,
+  56,
+  50,
+  9,
+  13,
+  45
+];
+
+function sockMerchant(n, ar) {
+  let stringAr = ar.map(String)
+  let uniqueAr = [];
+  let obj = new Object()
+  for (let i = 0; i < n; i++) {
+    if (uniqueAr.includes(stringAr[i]) === false) {
+      uniqueAr.push(stringAr[i]);
+    }
+  }
+  for (let i = 0; i < uniqueAr.length; i++) {
+    // console.log(uniqueAr[i])
+    obj.uniqueAr[i] = []
+  }
+  // console.log(uniqueAr);
+  console.log(obj)
+}
+
+sockMerchant(n, ar);

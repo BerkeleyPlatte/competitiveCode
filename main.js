35ab95bc15b1c57919633980b1f1a1afc9c3a6ae
berkeley platte <berkeleyplatte989@gmail.com>
2019-09-12 13:49:58 -0500

    first hackerRank challange solved

--- main.js
+++ main.js
@@ -1,122 +1,41 @@
-let n = 100;
-let ar = [
-  50,
-  49,
-  38,
-  49,
-  78,
-  36,
-  25,
-  96,
-  10,
-  67,
-  78,
-  58,
-  98,
-  8,
-  53,
-  1,
-  4,
-  7,
-  29,
-  6,
-  59,
-  93,
-  74,
-  3,
-  67,
-  47,
-  12,
-  85,
-  84,
-  40,
-  81,
-  85,
-  89,
-  70,
-  33,
-  66,
-  6,
-  9,
-  13,
-  67,
-  75,
-  42,
-  24,
-  73,
-  49,
-  28,
-  25,
-  5,
-  86,
-  53,
-  10,
-  44,
-  45,
-  35,
-  47,
-  11,
-  81,
-  10,
-  47,
-  16,
-  49,
-  79,
-  52,
-  89,
-  100,
-  36,
-  6,
-  57,
-  96,
-  18,
-  23,
-  71,
-  11,
-  99,
-  95,
-  12,
-  78,
-  19,
-  16,
-  64,
-  23,
-  77,
-  7,
-  19,
-  11,
-  5,
-  81,
-  43,
-  14,
-  27,
-  11,
-  63,
-  57,
-  62,
-  3,
-  56,
-  50,
-  9,
-  13,
-  45
-];
+let n = 9;
+let ar = [10, 20, 20, 10, 10, 30, 50, 10, 20];
 
 function sockMerchant(n, ar) {
-  let stringAr = ar.map(String)
+  console.log(ar);
+  let stringAr = ar.map(String);
+  console.log(stringAr);
   let uniqueAr = [];
-  let obj = new Object()
+  let obj = {};
   for (let i = 0; i < n; i++) {
     if (uniqueAr.includes(stringAr[i]) === false) {
       uniqueAr.push(stringAr[i]);
     }
   }
+  console.log(uniqueAr);
   for (let i = 0; i < uniqueAr.length; i++) {
-    // console.log(uniqueAr[i])
-    obj.uniqueAr[i] = []
+    obj[uniqueAr[i]] = [];
   }
-  // console.log(uniqueAr);
-  console.log(obj)
+  // console.log(obj);
+  Object.keys(obj).forEach(key => {
+    for (let i = 0; i < n; i++) {
+      if (key === stringAr[i]) {
+        obj[key].push(stringAr[i]);
+      }
+    }
+  });
+  // console.log(obj);
+  let count = 0;
+  Object.values(obj).forEach(value => {
+    if (value.length % 2 !== 0) {
+      value.pop();
+    }
+    let num = value.length / 2;
+    count += num;
+  });
+  // console.log(obj);
+  console.log(count);
+  return count;
 }
 
 sockMerchant(n, ar);
